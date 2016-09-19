44da3f11bc90d126cf49366559d69c0453a5113c
Cameron Sowder <sowderca@users.noreply.github.com>
2016-09-19 14:09:42 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,8 @@
-Import-Module PSReadline
 Set-PSReadlineOption -EditMode Vi
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Get-PSSnapin -Name *Remote.Desktop* -Registered | % { Write-Host "$_..." -NoNewline;
-if ( (Get-PSSnapin -Name $_ -ErrorAction SilentlyContinue) -eq $null )
- { Write-Host "installing" -ForegroundColor Yellow; Add-PsSnapin $_ }
-else
- { Write-Host "already installed" -ForegroundColor Green; } }
-[string]$command = '"C:\Program Files\Vim\vim74\gvim.exe" --windowid !ConEmuBackHWND! -new_console:s50H';
-[String]$prompt = ">"
-function global:Windows-Vim {
-    Invoke-Expression "& $command"
+if ($IsLinux) {
+    Set-Alias vim nvim
 }
-Set-Alias vim Windows-Vim
+[char]$prompt = 0x276F
 function prompt {
-    $prompt
+    $prompt + " "
 }
