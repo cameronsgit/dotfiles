01b424e2ec75e6f97da4d0c8adfdf6ed1ef8a146
Cameron Sowder <sowderca@users.noreply.github.com>
2017-06-02 16:00:25 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,16 @@
 Set-PSReadlineOption -EditMode Vi
-if ($IsLinux) {
-    Set-Alias vim nvim
+Set-PSReadlineOption -BellStyle None
+Set-PSReadlineOption -ViModeIndicator Prompt
+
+if ($IsLinux -Or $IsOSX) {
+	Set-Alias vim nvim
+} else {
+	Set-Alias less more
 }
+
+# this char is not supported with default fonts for most platforms
 [char]$prompt = 0x276F
+
 function prompt {
-    $prompt + " "
+	$prompt + " "
 }
