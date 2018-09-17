2139a936a1c7a95973fdd7f7a83fcaae444d16f8
Cameron Sowder <cameron@sowder.io>
2018-09-17 12:11:10 -0400

    Cleaned up sync script     - Added some changes suggested by shellcheck

--- local/bin/sync-devwork.sh
+++ local/bin/sync-devwork.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-
-# -*- mode: sh -*-
-# vi: set ft=sh :
-
-printf "\e[32m Switching directories...\e[0m \n"
-pushd $HOME/Development
-printf "\e[32m Updating git reposititories...\e[0m \n"
-ls | xargs -P 10 -I {} git -C {} pull origin master
-printf "\e[32m Switching back...\e[0m \n"
-popd
-printf "\e[32m Done! \e[0m \n"
+#!bin/bash
+printf "\\e[32m Switching directories...\\e[0m \\n"
+pushd ~/Development || exit
+printf "\\e[32m Updating git repos...\\e[0m \\n"
+find "$(pwd)" -type d -depth 1 | xargs -P10 -I{} hub -C {} sync
+printf "\\e[32m Switching back...\\e[0m \\n"
+popd || exit
+printf "\\e[32m Done! \\e[0m \\n"
