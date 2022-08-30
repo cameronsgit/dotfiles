8343d5a6dc6b6eea2c5809b5d9e64d83e736900a
Cameron Sowder <cameron@sowder.io>
2022-08-30 16:24:48 -0400

    re-worked crawl script to support online playing and viewing over ssh

--- local/bin/crawl.zsh
+++ local/bin/crawl.zsh
@@ -1,7 +1,23 @@
 #!/usr/bin/env zsh
 
-function dcss {
-    pushd "/Applications/Dungeon Crawl Stone Soup - Console.app/Contents/Resources/"
-    ./crawl &
-    popd
+# usage
+# crawl => play my local install version
+# crawl -online => watch live games ppl are playing or play one myself
+
+
+# TODO: prbly should re-write to pwsh so I can use it on Windows as well.
+
+
+function crawl {
+    local online_user="crawl"
+    local online_host="crawl.develz.org"
+    local binary_path="/Applications/Dungeon Crawl Stone Soup - Console.app/Contents/Resources/"
+    if [ -n "$1" ]
+    then
+        # Play / watch online
+        ssh "${online_user}@${online_host}"
+    else
+        # Play locally
+        "${binary_path}/crawl" &
+    fi
 }
